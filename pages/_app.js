042df3212527f117abc6a665bc4b40548fff2957
luandro <luandro@gmail.com>
2019-08-05 12:34:58 -0300

    updated deps and added page transition lib

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,45 @@
-import App, { Container } from 'next/app'
-import React from 'react'
-import withApolloClient from '../lib/with-apollo-client'
-import { ApolloProvider } from 'react-apollo'
+import App, { Container } from "next/app"
+import React from "react"
+import withApolloClient from "../lib/with-apollo-client"
+import { ApolloProvider } from "react-apollo"
+import { PageTransition } from "next-page-transitions"
 
 class MyApp extends App {
-  render () {
-    const { Component, pageProps, apolloClient } = this.props
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps, route: router.route }
+  }
+
+  render() {
+    const { Component, pageProps, route, apolloClient } = this.props
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
+          <PageTransition timeout={300} classNames='page-transition'>
+            <Component {...pageProps} key={route} />
+          </PageTransition>
         </ApolloProvider>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transition: opacity 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}</style>
       </Container>
     )
   }
