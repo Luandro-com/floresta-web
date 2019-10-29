f7d9f11304e13b929f92b4afbb38b4c445160205
luandro <luandro@gmail.com>
2019-10-29 15:18:00 -0300

    possible bback button fix, but total fix not possible

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,11 +1,15 @@
-import Router from "next/router"
-import Arrows from "./Arrows"
-import colors from "../lib/colors"
+import Router from 'next/router'
+import Arrows from './Arrows'
+import colors from '../lib/colors'
 
-export default function BackButton({ to, top, left }) {
+export default function BackButton ({ to, top, left }) {
+  let hasGoBack = false
+  if (process.browser && document && document.referrer !== '') {
+    hasGoBack = true
+  }
   return (
     <button onClick={() => (to ? Router.push(to) : Router.back())}>
-      <Arrows size='10px' left top={top || "64%"} left={left || "17px"} />
+      <Arrows size='10px' left top={top || '64%'} left={left || '17px'} />
       Voltar
       <style jsx>{`
         cursor: pointer;
@@ -14,7 +18,7 @@ export default function BackButton({ to, top, left }) {
         padding: 12px 25px 12px 50px;
         border-radius: 10px;
         color: ${colors.light};
-        display: flex;
+        display: ${hasGoBack ? 'flex' : 'none'};
         flex-flow: row nowrap;
         align-items: center;
       `}</style>
